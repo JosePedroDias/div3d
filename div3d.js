732945a446a351d6b124e8484e7b2c3c2bdaa666
José Pedro Dias <jose.pedro.dias@gmail.com>
2012-12-14 02:01:25 +0000

    added glmatrix and started sample 2

--- div3d.js
+++ div3d.js
@@ -3,6 +3,8 @@ var div3d = (function() {
     return {
 
         _applyMatrix: function(el, mtx) {
+        	mtx = mat4.str(mtx);
+        	//console.log(mtx);
             el.style.webkitTransform = ['matrix3d(', mtx, ')'].join('');
         }
 
